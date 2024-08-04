9b035e109702856ff1f7da142819a785e6958a6f
rogeriobmoraes <85852718+rogeriobmoraes@users.noreply.github.com>
2024-08-04 09:56:53 +0000

    Atualizacao do curso CS50

---
+++ hello.py
@@ -0,0 +1,6 @@
+def hello(to):
+    print("hello,", to)    
+
+
+name = input("What's your name? ")
+hello (name) 
---
+++ soma.py
@@ -0,0 +1,6 @@
+def soma(a, b): # a e b sao os parametros a serem preenchidos pelos argumentos 
+    return a + b    
+resultado = soma(3, 5)  # Aqui, 3 e 5 são os argumentos passados para a função 'soma'
+
+
+print(resultado)  # Saída: 8
